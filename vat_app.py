0bcb1d3466f691299ec10a1a2f6d3927638e459d
411-42-Thatchakorn <56874@yupparaj.ac.th>
2026-08-06 14:14:25 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -5,5 +5,5 @@ vat = price * 0.07
 net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5  ม.4/5")
+st.write("นางสาวธัชกร สุทธิจำนงค์ เลขที่ 42  ม.4//11")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
